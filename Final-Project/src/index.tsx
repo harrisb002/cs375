b1c9bd7de1f95572206cd2346882029fe3123bf9
harrisb002 <harrisbe@sonoma.edu>
2024-12-07 19:21:49 -0800

    adding utility functions/ constants

---
+++ Final-Project/src/index.tsx
@@ -0,0 +1,10 @@
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
