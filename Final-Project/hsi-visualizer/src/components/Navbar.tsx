3a8bd9bc30b95c539604abf8dfd5923f44372f37
harrisb002 <harrisbe@sonoma.edu>
2024-12-09 14:58:32 -0800

    Going at it differently

--- Final-Project/hsi-visualizer/src/components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Navbar: React.FC = () => (
-  <nav style={{ background: '#1E293B', padding: '10px', color: '#fff' }}>
-    <h1 style={{ margin: 0, fontSize: '24px' }}>HSI Visualizer</h1>
-  </nav>
-);
-
-export default Navbar;
--- Final-Project/hsi-visualizer/src/types/HSIData.ts
+++
@@ -1,5 +0,0 @@
-export interface HSIData {
-    frequency: number;
-    intensity: number;
-  }
-  
